d475b863cc073d511cab3bd961791b9a38813049
MANIKANTA\tamma <tammamanikantareddy@gmail.com>
2026-05-02 23:44:50 -0500

    Thread implementation

--- src/MulThrd.java
+++ src/MulThrd.java
@@ -4,6 +4,9 @@ public class MulThrd extends Thread
         System.out.println("Thread is running");
     }
     public static void main(String[] args){
+        MulThrd td = new MulThrd();
+        td.start();
+        td.run();
 
     }
 }
